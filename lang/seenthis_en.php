3f157079bfd1f444ceece3069e012e9a5353d960
Fil <fil@rezo.net>
2012-10-01 09:25:41 +0000

    entetes de copyright n'ayant rien a voir avec seenthis
    git-svn-id: svn://trac.rezo.net/seenthis/squelettes@232 726c7f6e-f4ed-4363-883b-90581dc1cf32

--- lang/seenthis_en.php
+++ lang/seenthis_en.php
@@ -1,18 +1,6 @@
 <?php
 
 
-	/**
-	 * SPIP-Sondages
-	 *
-	 * Copyright (c) 2006-2009
-	 * Agence Art&eacute;go http://www.artego.fr
-	 *  
-	 * Ce programme est un logiciel libre distribue sous licence GNU/GPLv3.
-	 * Pour plus de details voir http://www.gnu.org/licenses/gpl-3.0.html
-	 *  
-	 **/
-
-
 	$GLOBALS[$GLOBALS['idx_lang']] = array(
 		'accueil' => 'Home',
 		"people" => "people",
