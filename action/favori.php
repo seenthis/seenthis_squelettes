b9dea6709923fedfb69466aab7c3589198789b0c
Fil <fil@rezo.net>
2013-01-10 11:23:34 +0000

    mieux controler l'action de partage : prendre en compte un parametre share=1 ou -1 qui indique si on veut ajouter ou supprimer le partage ; partager le parent si on envoie une demande sur un enfant, verifier l'existence du message partage
    git-svn-id: svn://trac.rezo.net/seenthis/squelettes@298 726c7f6e-f4ed-4363-883b-90581dc1cf32

--- action/favori.php
+++ action/favori.php
@@ -4,16 +4,28 @@
 function action_favori_dist() {
 
 	$auteur_session = $GLOBALS["visiteur_session"]["id_auteur"];
-	$id_me = floor(_request("id_me"));
 
 	if ($auteur_session < 1) exit;
-	#cache_me($id_me);
 
-	$query = sql_select("*", "spip_me_share", "id_me=$id_me AND id_auteur=$auteur_session");
-	if (sql_fetch($query)) {
-		//ECHO "SUPPRIMER";
-		sql_query("DELETE FROM spip_me_share WHERE id_me=$id_me AND id_auteur=$auteur_session");
-	} else {
+	// on verifie si le msg existe ; le cas echeant, on partagera son id_parent
+	$me = sql_allfetsel('id_me,id_parent', 'spip_me', 'id_me='.sql_quote(_request("id_me")));
+	if (!$me)
+		return;
+	if ($me[0]['id_parent'])
+		$id_me = $me[0]['id_parent'];
+	else
+		$id_me = $me[0]['id_me'];
+
+	// il est deja partage ?
+	$deja = count($a = sql_allfetsel("*", "spip_me_share", "id_me=$id_me AND id_auteur=$auteur_session"));
+
+	// veut-on "partager" ou "arreter de" ?
+	// si on ne precise pas, ca bascule
+	if (!$share = intval(_request('share')))
+		$share = $deja ? -1 : +1;
+
+	// partager : ajouter un lien si pas deja
+	if ($share == 1 AND !$deja) {
 		//echo "AJOUTER";
 		sql_insertq("spip_me_share",
 			array(
@@ -22,6 +34,13 @@ function action_favori_dist() {
 			)
 		);
 	}
+
+	// partager : supprimer tous les liens
+	if ($share == -1 AND $deja) {
+		//echo "SUPPRIMER";
+		sql_query("DELETE FROM spip_me_share WHERE id_me=$id_me AND id_auteur=$auteur_session");
+	}
+
 	cache_me($id_me);
 	
 	$page = recuperer_fond("noisettes/afficher_message",array("id"=>$id_me),array('trim'=>false));
