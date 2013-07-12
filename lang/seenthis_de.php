44f997a5e330ffff5281d48e29508ddbd6dd474e
Salvatore <salvatore@rezo.net>
2013-07-12 03:37:16 +0000

    langues (seenthis)
    git-svn-id: svn://trac.rezo.net/seenthis/squelettes@518 726c7f6e-f4ed-4363-883b-90581dc1cf32

---
+++ lang/seenthis_de.php
@@ -0,0 +1,118 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/seenthis?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'Z' => 'ZZzZZzzz',
+
+	// A
+	'accueil' => 'Eigener Stream',
+	'annuler' => 'Abbrechen',
+	'auteur_block' => '@people@ ignorieren',
+	'auteur_block_you' => 'Cet auteur vous a bloqué. Vous ne pouvez pas commenter ses messages.', # NEW
+	'auteur_ne_plus_block' => '@people@ nicht mehr ignorieren',
+	'auteur_ne_plus_suivre' => '@people@ entfolgen',
+	'auteur_vous_block' => '@people@ wird fortan ignoriert',
+	'auteur_vous_suivez' => 'Sie folgen jetzt @people@',
+	'auteurs_vous_suivez' => 'Ihre Kontakte',
+
+	// B
+	'beta_publique' => '<strong>Seenthis est en beta publique.</strong><br>Vous pouvez vous inscrire et participer, merci pour votre patience et votre bonne volonté durant cette phase.', # NEW
+	'bookmarklet_descriptif' => 'Pour l’utiliser : lorsque vous visitez la page d’un article que vous souhaitez référencer, sélectionnez l’extrait du texte à citer, et cliquez sur votre bookmarklet. Votre interface Seenthis s’ouvre alors, et votre message est pré-rempli avec le titre de la page, l’URL et la citation.', # NEW
+	'bookmarklet_titre' => 'Faites glisser ce bookmarklet dans votre barre de favoris :', # NEW
+
+	// E
+	'ecrire_commentaire' => 'Kommentieren',
+	'effacer' => 'Löschen',
+	'envoyer' => 'Absenden (Umschalt+Eingabe)',
+
+	// F
+	'favori' => 'Weitersagen',
+
+	// I
+	'insecable_typo' => ' ', # NEW
+	'intitule_connexion' => 'Anmelden',
+
+	// L
+	'logout' => 'Abmelden',
+
+	// M
+	'me_suggerer_contacts' => 'Kontaktvorschläge',
+	'message_inscription' => '<strong>Inscription gratuite et immédiate</strong><br>Dès l’enregistrement, votre mot de passe vous est expédié par email.', # NEW
+	'message_suggerer' => '
+				<strong>Nouveau sur Seenthis ? Vous devriez commencer par suivre des auteurs.</strong>
+				<p>Cliquez sur le bouton « Me suggérer des contacts » ci-contre, et 
+				visitez les pages des auteurs proposés. Si un auteur vous intéresse, suivez-le en cliquant, 
+				en haut de page, sur « Suivre cet auteur ».
+				Puis revenez sur cette page, recommencez l\'opération, les propositions s\'affineront au fur et
+				à mesure de votre propre sélection d\'auteurs.</p>
+				<p>Ce message disparaîtra lorsque vous suivrez cinq auteurs. 
+				Rendez-vous ensuite  sur l\'onglet « @auteurs » pour obtenir des suggestions d\'auteurs.</p>', # NEW
+	'modifier' => 'Bearbeiten',
+	'montrer_messages' => 'montrer les @total@ messages', # NEW
+
+	// P
+	'pave_accueil' => 'Du <strong>short-blogging</strong> sans limite de caractères. De la <strong>recommandation de liens</strong>. Des <strong>automatismes</strong> pour rédiger facilement vos messages. Des <strong>forums</strong> sous chaque billet. De la <strong>veille d’actualité</strong>. Une <strong>thématisation</strong> avancée.', # NEW
+	'people' => 'Kontakte',
+	'profil' => 'Einstellungen',
+	'profil_alerte_conversations' => '<b>conversations</b> / quelqu’un répond à un billet auquel j’ai moi-même répondu', # NEW
+	'profil_alerte_dubien' => '<b>un ami qui vous veut du bien</b> / quelqu’un me suit', # NEW
+	'profil_alerte_nolife' => '<b>nolife</b> / un auteur que je suis répond à n’importe quel billet', # NEW
+	'profil_alerte_nouveaux_billets' => '<b>nouveaux billets</b> / un nouveau billet est posté par un auteur que je suis', # NEW
+	'profil_alerte_reponses' => '<b>réponses à mes billets</b> / quelqu’un répond à un de mes billets', # NEW
+	'profil_alertes' => 'Benachrichtigungen',
+	'profil_copyleft' => 'Copyright, copyleft',
+	'profil_copyright_classique' => 'Pas de licence spécifique (droits d’auteur par défaut)', # NEW
+	'profil_couleur' => 'Ihre Farbeinstellung',
+	'profil_entete' => 'Bandeau supérieur (960 pixels de large)', # NEW
+	'profil_fond' => 'Image de fond (1600px x 1200px au maximum)', # NEW
+	'profil_graphisme' => 'Individuelle graphische Gestaltung',
+	'profil_identite' => 'Persönliche Angaben',
+	'profil_langue' => 'Sprachauswahl',
+	'profil_licence' => 'Lizenz für Ihre Beiträge',
+	'profil_logo' => 'Avatar (quadratisch)',
+	'profil_mexpedier' => 'M’expédier un courrier<br> électronique quand...', # NEW
+	'profil_rss' => 'Importer automatiquement un flux d\'articles au format ATOM ou RSS', # NEW
+
+	// R
+	'raccourci_bold' => '*<b>fett</b>*',
+	'raccourci_italic' => '_<i>kursiv</i>_',
+	'raccourci_quote' => '❝Zitat❞ (Umschalt+Tab)',
+	'raccourci_strike' => '-<del>durchgestrichen</del>-',
+
+	// S
+	'slogan_lien' => 'Inscrivez-vous, c’est gratuit et rapide !', # NEW
+	'slogan_texte' => '<strong>Participez à la discussion !</strong> Sur Seenthis, vous pouvez référencer des articles, les commenter, écrire des billets, discuter avec vos amis, suivre les auteurs qui vous intéressent...', # NEW
+	'suggestions' => 'Vorschläge:',
+	'suivre_people' => 'Suivre @people@', # NEW
+	'suivre_url' => 'Suivre ce site', # NEW
+	'suivre_url_stop' => 'Sie entfolgen diese Internetseite',
+	'suivre_url_you' => 'Sie folgen dieser Internetseite',
+
+	// T
+	'tags' => 'Stichwörter',
+	'theme_automatiquement' => 'Ce <b>thème</b> a été généré <b>automatiquement</b>.', # NEW
+	'theme_manuellement' => 'Ce <b>thème</b> est attribué <b>manuellement</b> par les auteurs des messages.', # NEW
+	'themes_automatiques' => 'thèmes automatiques', # NEW
+	'themes_vous_suivez' => 'Stichwörtern, denen Sie folgen',
+	'tous_les_messages_de' => 'Alle Beiträge von @people@',
+	'traduire_avec_google' => 'Übersetzten',
+
+	// U
+	'urls' => 'WWW-Auswahl (URLs)',
+	'urls_vous_suivez' => 'WWW-Seiten, denen Sie folgen',
+
+	// V
+	'votre_message' => 'Ihr Beitrag',
+	'votre_message_public' => 'Votre message public à ', # NEW
+	'vous_connaissez' => 'Vous connaissez peut-être :', # NEW
+	'vous_suivent' => 'Ihnen folgen',
+	'vous_suivez' => 'Sie folgen'
+);
+
+?>
