50a5c87c6e2503da1df65c593816830a03fa3e0b
Salvatore <salvatore@rezo.net>
2013-08-06 03:41:03 +0000

    langues (seenthis)
    git-svn-id: svn://trac.rezo.net/seenthis/squelettes@537 726c7f6e-f4ed-4363-883b-90581dc1cf32

--- lang/seenthis_no.php
+++ lang/seenthis_no.php
@@ -12,25 +12,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'accueil' => 'Forside',
-	'annuler' => 'avbryte',
-	'auteur_block' => 'blokkere @people@',
+	'annuler' => 'Avbryt',
+	'auteur_block' => 'Blokker @people@',
 	'auteur_block_you' => 'Denne bidragsyteren har blokkert deg. Du kan ikke kommentere innleggene hans',
-	'auteur_ne_plus_block' => 'låse opp @people@',
-	'auteur_ne_plus_suivre' => 'Ikke følg mere @people@',
+	'auteur_ne_plus_block' => 'Lås opp @people@',
+	'auteur_ne_plus_suivre' => 'Slutt å følge @people@',
 	'auteur_vous_block' => 'Du har blokkert @people@',
 	'auteur_vous_suivez' => 'Du følger @people@',
 	'auteurs_vous_suivez' => 'Bidragsytere du følger',
 
 	// B
-	'beta_publique' => '<strong>Seenthis er i offentlige testing
-</strong><br>Du kan registrere deg og delta : takk for din tålmodighet og "goodwill" i løpet av denne fasen.',
-	'bookmarklet_descriptif' => 'Om du ønsker å bruke det : Når du besøker en side av en artikkel som du vil referere til, velger du tekstutdragen å sitere, og klikk på "bookmarklet". Din Seenthis side åpnes, og meldingen er forhåndsutfylt med siden tittelen, URL, og sitaten.',
+	'beta_publique' => '<strong>Seenthis er i beta-versjon.</strong><br>Du kan registrere deg og delta: Takk for din tålmodighet og godvilje i løpet av denne fasen.',
+	'bookmarklet_descriptif' => 'For å bruke det: Når du er på siden til en artikkel som du vil referere til, velger du tekstutdraget du ønsker å sitere og klikker på "bookmarklet". Din Seenthis-side åpnes med en forhåndsutfylt melding med sidens tittelen, URL og sitatet.',
 	'bookmarklet_titre' => 'Dra denne bookmarklet til bokmerkelinjen',
 
 	// E
 	'ecrire_commentaire' => 'Skriv en kommentar',
 	'effacer' => 'slett',
-	'envoyer' => 'send (kap+linjeskift)',
+	'envoyer' => 'Send (kap+linjeskift)',
 
 	// F
 	'favori' => 'Dele',
@@ -43,40 +42,39 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'logout' => 'Logg ut',
 
 	// M
-	'me_suggerer_contacts' => 'Foreslå meg noen nye kontakter',
-	'message_inscription' => '<strong>Medlemskap er gratis og umiddelbar</strong><br>D Ved registrering vil passordet bli sendt via e-post.',
-	'message_suggerer' => '				Ny på <strong> Seenthis ?   Du bør starte med følgende bidragsytere. </ Strong>
-				<p><p> Klikk på "foreslå meg noen bidragsytere"-knappen, og besøke sidene av bidragsytere foreslått. Om du er interessert i en bidragsyter kan du følge den ved å klikke
-Opp på "Følg denne bidragsyter."
-				Gå deretter tilbake til denne siden, prøv dette på nytt, forslagene kommer til å bli mer og mer presis. </ p>
+	'me_suggerer_contacts' => 'Foreslå kontakter',
+	'message_inscription' => '<strong>Medlemskap er gratis og umiddelbart</strong><br>Passord blir umiddelbart sendt til din e-postadresse.',
+	'message_suggerer' => '				<strong>Ny på Seenthis? Du bør starte med følgende bidragsytere.</ Strong>
+				<p><p> Klikk på knappen "foreslå bidragsytere" for å se sidene til de foreslåtte bidragsyterne. Du kan følge bidragsytere med å klikke på "Følg denne bidragsyter."
+				Gå deretter tilbake til denne siden, prøv dette på nytt, forslagene vil bli stadig mer presise. </ p>
 				<p>Denne meldingen forsvinner når du følger fem bidragsyter.
-				Deretter går du til fanen "@ bidragsyter" for å få forslag til bidragsyter</p>',
+				Deretter går du til fanen "@bidragsyter" for å få forslag til bidragsyter</p>',
 	'modifier' => 'endre',
-	'montrer_messages' => 'vis alle @total@ Innleggene',
+	'montrer_messages' => 'vis alle @total@ innlegg',
 
 	// P
-	'pave_accueil' => '<strong> "Short-blogging" </ strong> ubegrenset tegn. <strong> Anbefalingen lenker </ strong>. automatisering <strong> </ strong> for enkelt å skrive inlegg. <strong> Forums </ strong> på hvert inlegg. <strong> Like før nyheten </ strong>. En avansert <strong> tematisere </ strong> .',
-	'people' => 'bidragsyter',
+	'pave_accueil' => '<strong> "Short-blogging" </ strong> ubegrenset antall tegn. <strong> Anbefalte lenker </ strong>. Automatiseringer <strong> </ strong> for å skrive innlegg enkelt. <strong> Forum </ strong> under hvert innlegg. <strong> Like før nyheten </ strong>. Avansert <strong>tema</ strong>.',
+	'people' => 'bidragsytere',
 	'profil' => 'innstillinger',
-	'profil_alerte_conversations' => '<b> samtaler </ b> / noen svarer på en inlegg som jeg selv svarte',
-	'profil_alerte_dubien' => '<b> en venn som ønsker deg godt </ b> / noen følger meg',
-	'profil_alerte_nolife' => '<b>"nolife"</b> /  En bidragsyter som jeg føler svarer på noen inlegg',
-	'profil_alerte_nouveaux_billets' => '<b>nye inlegg</b> /et nytt innlegg er lagt ut av en bidragsyter jeg føler
+	'profil_alerte_conversations' => '<b> samtaler </ b> / noen svarer på et innlegg jeg har svart på',
+	'profil_alerte_dubien' => '<b>en venn som ønsker deg godt</ b> / noen følger meg',
+	'profil_alerte_nolife' => '<b>"nolife"</b> /  En bidragsyter jeg føler svarer på et innlegg',
+	'profil_alerte_nouveaux_billets' => '<b>nye inlegg</b> /et nytt innlegg er lagt ut av en bidragsyter jeg følger
 ',
 	'profil_alerte_reponses' => '<b> svar på mine meldinger </ b> / noen svarer på en av mine meldinger',
 	'profil_alertes' => 'Varsler',
 	'profil_copyleft' => 'Copyright, copyleft',
-	'profil_copyright_classique' => 'Ingen spesiell lisens (opphavsrettigheter som standard)',
+	'profil_copyright_classique' => 'Ingen spesifikk lisens (opphavsrett er standard)',
 	'profil_couleur' => 'Din farge',
 	'profil_entete' => 'Toppbanner (960 px bredt)',
 	'profil_fond' => 'Bakgrunnsbilde (1600 px x 1200 px maksimum)',
-	'profil_graphisme' => 'oppsettet ditt',
+	'profil_graphisme' => 'Ditt oppsett',
 	'profil_identite' => 'ID',
-	'profil_langue' => 'Min språk',
-	'profil_licence' => 'Lisens til innleggene dine',
+	'profil_langue' => 'Mitt språk',
+	'profil_licence' => 'Dine innleggs lisens',
 	'profil_logo' => 'logo bidragsyter (firekant)',
 	'profil_mexpedier' => 'Send meg en e-post <br>når  ...',
-	'profil_rss' => 'Automatisk importere en strøm av artikler i RSS eller ATOM',
+	'profil_rss' => 'Importer automatisk artikler med RSS eller ATOM',
 
 	// R
 	'raccourci_bold' => '*<b>fet skrift</b>*',
@@ -85,18 +83,18 @@ Opp på "Følg denne bidragsyter."
 	'raccourci_strike' => '-<del>gjennomstreking</del>-',
 
 	// S
-	'slogan_lien' => 'Meld deg, dette er gratis og enkelt !',
-	'slogan_texte' => '<strong>PBli med i diskusjonen !</strong> På Seenthis, kan du referere artikler, kommentere, skrive notater, diskutere med venner, følge bidragsyteren du er interessert i...',
-	'suggestions' => 'Anbefalinger :',
+	'slogan_lien' => 'Registrer deg, det er gratis og enkelt!',
+	'slogan_texte' => '<strong>Bli med i diskusjonen!</strong> På Seenthis kan du henvise til artikler, kommentere, skrive notater, diskutere med venner og følge de bidragsyterne du er interessert i ...',
+	'suggestions' => 'Anbefalinger:',
 	'suivre_people' => 'Følg @people@',
-	'suivre_url' => 'Følg denne nettside',
+	'suivre_url' => 'Følg denne nettsiden',
 	'suivre_url_stop' => 'Ikke følg denne nettside lenger',
 	'suivre_url_you' => 'Du følger denne nettside',
 
 	// T
 	'tags' => 'temaer',
-	'theme_automatiquement' => 'Dette <b>temaet</ b> ble <b>automatisk</ b> generert.',
-	'theme_manuellement' => 'Dette <b>temaet</b>  tilordnes <b>manuelt</b> av bidragsytere meldinger.
+	'theme_automatiquement' => 'Dette <b>temaet</ b> er <b>automatisk</ b> generert.',
+	'theme_manuellement' => 'Dette <b>temaet</b>  tilordnes <b>manuelt</b> av meldingenes bidragsytere.
 
 ',
 	'themes_automatiques' => 'Automatiske temaer',
@@ -104,7 +102,7 @@ Opp på "Følg denne bidragsyter."
 
 ',
 	'tous_les_messages_de' => 'Alle innleggene av @people@',
-	'traduire_avec_google' => 'oversette',
+	'traduire_avec_google' => 'oversett',
 
 	// U
 	'urls' => 'Nettsider',
@@ -112,8 +110,8 @@ Opp på "Følg denne bidragsyter."
 
 	// V
 	'votre_message' => 'Din melding',
-	'votre_message_public' => 'Din melding til alle',
-	'vous_connaissez' => 'Kansje er du kjent med :',
+	'votre_message_public' => 'Din offentlige melding til',
+	'vous_connaissez' => 'Kanskje kjenner du:',
 	'vous_suivent' => 'abonnenter',
 	'vous_suivez' => 'abonnementer'
 );
