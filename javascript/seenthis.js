dd40e136d466da04c4065f6bf9a5b9cfa1930077
Julien Kirch <code@archiloque.net>
2015-01-18 13:07:23 +0100

    Évite les clignotements lors de l'édition de textes quand il y a des images

--- javascript/seenthis.js
+++ javascript/seenthis.js
@@ -83,6 +83,17 @@ $.fn.afficher_masques = function () {
 };
 
 /**
+ * Les urls qu'on a déjà validées comme étant des images, pour éviter l'effet de clignotement
+ * @type {{}}
+ */
+var suivreEditionImagesValides = {};
+/**
+ * Les urls qu'on a déjà validées comme n'étant pas des images, pour éviter l'effet de clignotement
+ * @type {{}}
+ */
+var suivreEditionImagesInvalides = {};
+
+/**
  * Créé les images pour la fonction suivreEdition.
  * @param parentDiv le div parent où ajouter le html
  * @param imageUrl l'url de l'image à ajouter
@@ -92,12 +103,15 @@ $.fn.afficher_masques = function () {
  * */
 function suivreEditionCreateImage(parentDiv, imageUrl, elementLien, elementImages, elementsLiens){
 	var tmpImg = $('<img>').on('load', function(){
+		suivreEditionImagesValides[imageUrl] = true;
 		tmpImg.appendTo(parentDiv);
 		elementLien.remove();
 		elementImages.slideDown();
 		if(elementsLiens.find('.lien').length == 0) {
 			elementsLiens.slideUp('fast');
 		}
+	}).on('error', function(){
+		suivreEditionImagesInvalides[imageUrl] = true;
 	}).attr('src', imageUrl);
 }
 
@@ -148,20 +162,38 @@ $.fn.suivreEdition = function () {
 	imagesHtml.html("<div class='titre_images'>Images:</div>");
 	if (matchUrl) {
 		liensHtml.html(liens);
+		var nombreDeLiens = 0;
+		var nombreDImages = 0;
+
 		for (i = 0; i < matchUrl.length; ++i) {
 			var lienUrl = matchUrl[i];
-			var lienAff = lienUrl.replace(racine_url_match, "<span>$1</span>");
-			lienAff = lienAff.replace(fin_url_match, "<span>$1</span>");
 
-			var elementLien = $("<div class='lien'>⇧<a href=\"" + lienUrl + "\" class='spip_out'>" + lienAff + "</a></div>").appendTo(liensHtml);
-			// pour le cas où il s'agit d'une image
-			suivreEditionCreateImage(imagesHtml, lienUrl, elementLien, imagesHtml, liensHtml);
+			if(suivreEditionImagesValides[lienUrl]) {
+				nombreDImages++;
+				$("<img src=\"" + lienUrl + "\">").appendTo(imagesHtml)
+			} else {
+				nombreDeLiens++;
+				var lienAff = lienUrl.replace(racine_url_match, "<span>$1</span>");
+				lienAff = lienAff.replace(fin_url_match, "<span>$1</span>");
+				var elementLien = $("<div class='lien'>⇧<a href=\"" + lienUrl + "\" class='spip_out'>" + lienAff + "</a></div>").appendTo(liensHtml);
+				// si c'est un lien pas encore testé il peut s'agir d'une image
+				if(! suivreEditionImagesInvalides[lienUrl]) {
+					suivreEditionCreateImage(imagesHtml, lienUrl, elementLien, imagesHtml, liensHtml);
+				}
+			}
 		}
-		liensHtml.slideDown();
+		if(nombreDeLiens > 0) {
+			liensHtml.slideDown();
+		}
+		if(nombreDImages > 0) {
+			imagesHtml.slideDown();
+		} else {
+			imagesHtml.slideUp();
+		}
 	} else {
 		liensHtml.slideUp();
+		imagesHtml.slideUp();
 	}
-	imagesHtml.slideUp();
 
 };
 
