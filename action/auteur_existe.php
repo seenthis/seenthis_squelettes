1fd38b2b189e1354f5fd572e9b5a0001536811e4
Fil <fil@rezo.net>
2015-03-07 23:24:19 +0100

    nettoyage de la query

--- action/auteur_existe.php
+++ action/auteur_existe.php
@@ -8,7 +8,7 @@ function action_auteur_existe() {
 	if ($auteur_session < 1) exit;
 
 	$login_auteur = _request("login_auteur");
-	$nombre = sql_getfetsel('count(*)', "spip_auteurs where spip_auteurs.login = ".sql_quote($login_auteur)." && statut!='nouveau' && statut != 'poubelle'", '');
+	$nombre = sql_getfetsel('count(*)', 'spip_auteurs', [ "login = ".sql_quote($login_auteur), "statut!='nouveau'", "statut != '5poubelle'" ]);
 	header('Content-Type: text/json; charset=utf-8');
 	header("Cache-Control: public, max-age=3600");
 	echo json_encode(array('result' => $nombre == 1));
