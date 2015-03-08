12e88683d343bd14fafbad9babc4043709cc83c3
Julien Kirch <code@archiloque.net>
2015-03-08 17:12:59 +0100

    Supprimer du code commenté pour le bookmarklet

--- javascript/seenthis/seenthis.js
+++ javascript/seenthis/seenthis.js
@@ -142,11 +142,6 @@ $(function () {
             $(".formulaire_principal textarea").val(content);
         }
 	}
-	
-	// vider l'URL
-	//if(content != ''){
-	//	window.location.hash = '';
-	//}
 
     $('textarea').each(function(_, textArea){
         $(textArea).suivreEdition();
