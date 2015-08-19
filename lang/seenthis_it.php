6d57fae52381d25737c88c67d4a15f17160bba57
Salvatore <salvatore@arscenic.org>
2015-08-19 22:41:59 +0200

    [Salvatore] [source:trunk/lang/ seenthis] Export depuis http://trad.spip.net

---
+++ lang/seenthis_it.php
@@ -0,0 +1,129 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/seenthis?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'Z' => 'ZZzZZzzz',
+
+	// A
+	'accueil' => 'Home
+',
+	'annuler' => 'Annullare
+',
+	'auteur_block' => 'Bloccare @people@',
+	'auteur_block_you' => 'Questo autore ti ha bloccato. Non puoi commentare i suoi messaggi.',
+	'auteur_ne_plus_block' => 'Sbloccare @people@',
+	'auteur_ne_plus_suivre' => 'Smettere di seguire @people@',
+	'auteur_vous_block' => 'Hai bloccato @people@',
+	'auteur_vous_suivez' => 'Stai seguendo @people@',
+	'auteurs_vous_suivez' => 'Gli autori che segui',
+
+	// B
+	'beta_publique' => '<strong>Seenthis è nella versione beta pubblica.</strong><br />Puoi iscriverti e partecipare, grazie per la tua pazienza in questa fase.',
+	'bookmarklet_descriptif' => 'Per utilizzarlo: quando visiti la pagina con un articolo che vuoi citare, seleziona l’estratto del testo da citare, e clicca sul tuo bookmarklet. La tua interfaccia Seenthis si aprirà e il tuo messaggio sarà già compilato con il titolo della pagina, l’URL e la citazione.',
+	'bookmarklet_titre' => 'Trascina questo bookmarklet nella tua barra dei preferiti:',
+
+	// C
+	'changement_email_existe' => 'Questo indirizzo email è già utilizzato da un altro account.',
+	'changement_email_informer' => '@login@ ha indicato un nuovo indirizzo email: "@new@" (al posto di "@old@")',
+	'changement_email_subject' => 'Nuovo indirizzo email per @login@',
+
+	// E
+	'ecrire_commentaire' => 'Scrivere un commento',
+	'effacer' => 'cancellare',
+	'envoyer' => 'Invia (shift+enter)',
+
+	// F
+	'favori' => 'condividere',
+
+	// I
+	'insecable_typo' => ' ',
+	'intitule_connexion' => 'connessione',
+
+	// L
+	'logout' => 'disconnettersi',
+
+	// M
+	'me_suggerer_contacts' => 'Suggeriscimi dei contatti',
+	'message_inscription' => '<strong>Iscrizione gratuita e immediata</strong><br />La tua password ti sarà inviata via email al termine della registrazione.',
+	'message_suggerer' => '				<strong>Nuovo su seenthis? Dovresti cominciare col seguire un autore.</strong>
+				<p>Clicca sul pulsante "suggeriscimi un contatto" qui di fianco e visita le pagine degli autori che ti saranno proposti. Se un autore ti interessa, seguilo cliccando, 
+				in alto alla pagina, su "Seguire questo autore".
+				Poi torna su questa pagina e ricomincia l’operazione, le proposte si perfezioneranno progressivamente a seconda della tua scelta di autori.</p>
+				<p>Questo messaggio sparirà quando seguirai 5 autori. 
+				Ti diamo appuntamento su "@auteurs" per ottenere dei suggerimenti di autori.</p>',
+	'modifier' => 'modifica',
+	'montrer_messages' => 'mostra i @total@ messaggi',
+
+	// P
+	'pave_accueil' => 'Dello <strong>short-blogging</strong> senza limite di caratteri. Delle <strong>raccomandazioni di link</strong>. Degli <strong>automatismi</strong> per redigere facilmente i tuoi messaggi. Dei <strong>forums</strong> sotto ogni post. Delle <strong>rassegne stampa d’attualità</strong>. Una <strong>tematizzazione</strong> avanzata.',
+	'people' => 'autori',
+	'profil' => 'preferenze
+',
+	'profil_alerte_conversations' => '<b>conversazioni</b> / qualcuno risponde a un post a cui io stesso ho risposto',
+	'profil_alerte_dubien' => '<b>un amico che ti vuole bene</b> / qualcuno che mi segue',
+	'profil_alerte_mes_billets' => '<b>mie pubblicazioni</b> / ricevere una copia dei miei post ',
+	'profil_alerte_nolife' => '<b>senza vita</b> / un autore che seguo risponde a tutte le pubblicazioni',
+	'profil_alerte_nouveaux_billets' => '<b>nuovi post</b> / un nuovo post è stato pubblicato da un autore che seguo',
+	'profil_alerte_partage' => '<b>condivisione</b> / qualcuno ha condiviso il mio post',
+	'profil_alerte_reponse_partage' => '<b>risposta a una condivisione</b> / qualcuno ha rispoto a un post che ho condiviso',
+	'profil_alerte_reponses' => '<b>risposta ai miei post</b> / qualcuno ha risposto ai miei post',
+	'profil_alertes' => 'Avvisi',
+	'profil_copyleft' => 'Copyright, Copyleft',
+	'profil_copyright_classique' => 'Nessuna licenza specifica (diritti d’autore predefiniti)',
+	'profil_couleur' => 'Il tuo colore',
+	'profil_entete' => 'Banda superiore (960 px di larghezza)',
+	'profil_fond' => 'Immagine di sfondo (max 1600px x 1200px)',
+	'profil_graphisme' => 'La tua grafica',
+	'profil_identite' => 'Identità',
+	'profil_langue' => 'La mia lingua',
+	'profil_licence' => 'Licenza dei miei messaggi',
+	'profil_liens_partage_fb' => 'Mostra i bottoni di condivisione verso Facebook',
+	'profil_liens_partage_tw' => 'Mostra i bottoni di condivisione verso Twitter',
+	'profil_logo' => 'Logo dell’autore (quadrato)',
+	'profil_mexpedier' => 'Speditemi un email<br /> quando...',
+	'profil_partage' => 'Condividi',
+	'profil_rss' => 'Importare automaticamente un flusso d’articoli in formato ATOM o RSS',
+
+	// R
+	'raccourci_bold' => '*<b>grassetto</b>*',
+	'raccourci_italic' => '_<i>italico</i>_',
+	'raccourci_quote' => '❝citazione❞ (Shift+tab)',
+	'raccourci_strike' => '-<del>barrato</del>-',
+
+	// S
+	'slogan_lien' => 'Iscriviti, è gratuito e rapido!',
+	'slogan_texte' => '<strong>Partecipa alla discussione!</strong> Su Seenthis, puoi citare degli articoli, commentarli, scrivere dei post, discutere con i tuoi amici, seguire degli autori che ti interessano...',
+	'suggestions' => 'Suggerimenti:',
+	'suivre_people' => 'Seguire @people@',
+	'suivre_url' => 'Seguire questo sito',
+	'suivre_url_stop' => 'Smettere di seguire questo sito',
+	'suivre_url_you' => 'Stai seguendo questo sito',
+
+	// T
+	'tags' => 'temi',
+	'theme_automatiquement' => 'Questo <b>tema</b> è stato generato <b>automaticamente</b>.',
+	'theme_manuellement' => 'Questo <b>tema</b> è attribuito <b> manualmente</b> dagli autori dei messaggi.',
+	'themes_automatiques' => 'temi automatici',
+	'themes_vous_suivez' => 'I temi che segui',
+	'tous_les_messages_de' => 'Tutti i messaggi di @people@',
+	'traduire_avec_google' => 'tradurre',
+
+	// U
+	'urls' => 'siti',
+	'urls_vous_suivez' => 'I siti che segui',
+
+	// V
+	'votre_message' => 'Il tuo messaggio',
+	'votre_message_public' => 'Il tuo messaggio pubblico a',
+	'vous_connaissez' => 'Potresti conoscere:',
+	'vous_suivent' => 'abbonati',
+	'vous_suivez' => 'abbonamenti'
+);
+
+?>
