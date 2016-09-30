704098c77611d78c37975ea6cadf52a341267cd1
brunob <brunobergot@gmail.com>
2016-09-30 20:02:25 +0200

    passer le champ en_ligne de l'auteur à la date en cours lors du logout
    cf seenthis/seenthis_squelettes@c68482607386ab4c50a1d1874c00f9a4cad17dc0

--- seenthissq_pipelines.php
+++ seenthissq_pipelines.php
@@ -25,3 +25,23 @@ function seenthissq_post_insertion($flux){
 	}
 	return $flux;
 }
+
+/**
+ * Insertion dans le pipeline trig_auth_trace
+ * 
+ * Lors du logout d'un auteur, repasser la valeur du champ en_ligne à la date courante
+ * 
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @return array $flux
+ */
+function seenthissq_trig_auth_trace($flux){
+	if ($flux['args']['date'] == '0000-00-00 00:00:00') {
+		include_spip('inc/session');
+		// si c'est l'auteur actuellement connecté qui se déconnecte
+		if (session_get('id_auteur') and ($flux['args']['row']['id_auteur'] == session_get('id_auteur')) {
+			sql_updateq('spip_auteurs', array('en_ligne' => date('Y-m-d H:i:s')), "id_auteur=" . $flux['args']['row']['id_auteur']);
+		}
+	}
+	return $flux;
+}
