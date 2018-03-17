1e0b7c31af0dc467967a1bba864b6de2a3d884fc
Salvatore <salvatore@arscenic.org>
2018-03-17 05:36:59 +0100

    [Salvatore] [source:trunk/lang/ seenthis] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/seenthis_eo.php
@@ -0,0 +1,81 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/seenthis?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accueil' => 'Ĉefpâgo',
+	'annuler' => 'Nuligi',
+	'auteur_block' => 'Bloki @people@',
+	'auteur_block_you' => 'Tiu aŭtoro blokis vin. Vi ne povas komenti liajn aŭ ŝiajn mesaĝojn.',
+	'auteur_ne_plus_block' => 'Malbloki @people@',
+	'auteur_ne_plus_suivre' => 'Ne plu sekvi @people@',
+	'auteur_vous_block' => 'Vi blokis @people@',
+	'auteur_vous_suivez' => 'Vi sekvas @people@',
+	'auteurs_vous_suivez' => 'Aŭtoroj, kiujn vi sekvas',
+
+	// B
+	'beta_publique' => '<strong>Seenthis estas betaversio.</strong><br />Vi povas registriĝi kaj partopreni, dankon pro via pacienco kaj bonvolo dum tiu momento.',
+	'bookmarklet_descriptif' => 'Kiel uzi ĝin ? Dum vi vizitas artikolopaĝon, kiun vi deziras referenci, elektu ekstrakton de la citota teksto, kaj alklaku la legosignilon. Via „Seenthis“ interfaco tiam malfermiĝos, kaj via mesaĝo estos antaŭplenigita per la paĝotitolo, la retadreso kaj la citaĵo.',
+	'bookmarklet_titre' => 'Ŝovigi tiun legosignilon en via legosignobreton :',
+
+	// E
+	'ecrire_commentaire' => 'Skribi komenton',
+	'effacer' => 'forviŝi',
+	'envoyer' => 'Sendi (maj⇧+enig⏎)',
+
+	// F
+	'favori' => 'kunhavigi',
+
+	// I
+	'insecable_typo' => ' ',
+	'intitule_connexion' => 'konektiĝo',
+
+	// L
+	'logout' => 'malkonektiĝi',
+
+	// M
+	'me_suggerer_contacts' => 'Konsili al mi kontaktojn',
+	'modifier' => 'modifi',
+	'montrer_messages' => 'montri la @total@ mesaĝoj',
+
+	// P
+	'people' => 'aŭtoroj',
+	'profil' => 'agordoj',
+
+	// R
+	'raccourci_bold' => '*<b>grasa</b>*',
+	'raccourci_italic' => '_<i>kursiva</i>_',
+	'raccourci_quote' => '❝citaĵo❞ (maj⇧+tab⭾)',
+	'raccourci_strike' => '-<del>trastreka</del>-',
+
+	// S
+	'suggestions' => 'Sugestoj :',
+	'suivre_people' => 'Sekvi @people@',
+	'suivre_url' => 'Sekvi tiun retejon',
+	'suivre_url_stop' => 'Ne plu sekvi tiun retejon',
+	'suivre_url_you' => 'Vi sekvas tiun retejon',
+
+	// T
+	'tags' => 'temoj',
+	'themes_automatiques' => 'aŭtomataj temoj',
+	'tous_les_messages_de' => 'Ĉiuj mesaĝoj el @people@',
+	'traduire_avec_google' => 'traduki',
+
+	// U
+	'urls' => 'retejoj',
+	'urls_vous_suivez' => 'Retejoj, kiujn vi sekvas',
+
+	// V
+	'votre_message' => 'Via mesaĝo',
+	'votre_message_public' => 'Via publika mesaĝo al ',
+	'vous_connaissez' => 'Eble vi konas :',
+	'vous_suivent' => 'abonantoj',
+	'vous_suivez' => 'abonoj'
+);
