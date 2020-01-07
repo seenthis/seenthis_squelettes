e0580da338a11f2795446e301d69a93d946719bf
brunob <bruno@eliaz.fr>
2020-01-07 21:28:31 +0100

    renommer la variable recherchée plutôt que d'écraser $url
    au cas où on souhaiterait la réutiliser + un log de moins

--- action/messages_lien.php
+++ action/messages_lien.php
@@ -8,11 +8,10 @@ function action_messages_lien() {
 	if ($auteur_session < 1) exit;
 
 	$url = rawurldecode(_request("url"));
-	spip_log($url);
 	$url_messages = array();
 	// virer le http/https en début d'url + le slash final
-	$url = preg_replace(',/$,', '', preg_replace(',^(https?://)?,i', '', $url));
-	$id_possibles = sql_allfetsel('id_me', 'spip_me_tags', 'class = '.sql_quote('url').' and (tag = '.sql_quote('http://' . $url).' or tag = '.sql_quote('https://' . $url).')');
+	$lien_flou = preg_replace(',/$,', '', preg_replace(',^(https?://)?,i', '', $url));
+	$id_possibles = sql_allfetsel('id_me', 'spip_me_tags', 'class = '.sql_quote('url').' and (tag = '.sql_quote('http://' . $lien_flou).' or tag = '.sql_quote('https://' . $lien_flou).')');
 	$id_publies = sql_allfetsel(
 		'id_me',
 		'spip_me', array("statut = 'publi'", sql_in('id_me', array_map('array_pop', $id_possibles))));
