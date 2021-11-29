240f1ee165fe2e5e91a6d7093bee36aded08e8b4
Alberto <alm82@live.it>
2021-11-29 10:11:50 +0100

    [Salvatore] [source:lang/ seenthis] Export depuis https://trad.spip.net de la langue it [Salvatore] [source:lang/ seenthis] Mise a jour du bilan depuis https://trad.spip.net

--- lang/seenthis_it.php
+++ lang/seenthis_it.php
@@ -15,13 +15,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'accueil' => 'Home',
 	'annuler' => 'Annullare',
+	'aucun_message' => 'Nessun messaggio',
 	'auteur_block' => 'Bloccare @people@',
-	'auteur_block_you' => 'Questo autore ti ha bloccato. Non puoi commentare i suoi messaggi.', # MODIF
+	'auteur_block_you' => 'Questo autore ti ha bloccato. Non puoi commentare i suoi messaggi.',
 	'auteur_ne_plus_block' => 'Sbloccare @people@',
 	'auteur_ne_plus_suivre' => 'Smettere di seguire @people@',
 	'auteur_vous_block' => 'Hai bloccato @people@',
 	'auteur_vous_suivez' => 'Stai seguendo @people@',
-	'auteurs_vous_suivez' => 'Gli autori che segui', # MODIF
+	'auteurs_vous_suivez' => 'Gli autori che segui',
 
 	// B
 	'beta_publique' => '<strong>Seenthis è nella versione beta pubblica.</strong><br />Puoi iscriverti e partecipare, grazie per la tua pazienza in questa fase.',
@@ -40,6 +41,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
 	'favori' => 'condividere',
+	'flux_desactive_texte' => 'Buongiorno @nom@,
+L’importazione automatica del tuo feed RSS/ATOM è stata disabilitata perché è passato un po’ di tempo da quando hai effettuato l’accesso al sito.
+Puoi riattivarlo dalla tua pagina delle preferenze, cancellando la stella aggiunta all’inizio del suo indirizzo o cliccando sul seguente link @url@',
+	'flux_desactive_titre' => 'Importa il tuo feed disattivato su @nom_site@',
 
 	// I
 	'insecable_typo' => ' ',
@@ -49,29 +54,51 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'logout' => 'logout',
 
 	// M
+	'masquer_messages' => 'nascondi messaggi',
 	'me_suggerer_contacts' => 'Suggeriscimi dei contatti',
 	'message_inscription' => '<strong>Iscrizione gratuita e immediata</strong><br />La tua password ti sarà inviata via email al termine della registrazione.',
-	'message_suggerer' => '				<strong>Nuovo su seenthis? Dovresti cominciare col seguire un autore.</strong>
-				<p>Clicca sul pulsante "suggeriscimi un contatto" qui di fianco e visita le pagine degli autori che ti saranno proposti. Se un autore ti interessa, seguilo cliccando, 
+	'message_suggerer' => '				<strong>Nuovo? Dovresti cominciare col seguire un autore.</strong>
+				<p>Clicca sul pulsante "Suggeriscimi un contatto" qui di fianco e visita le pagine degli autori che ti saranno proposti. Se un autore ti interessa, seguilo cliccando, 
 				in alto alla pagina, su "Seguire questo autore".
 				Poi torna su questa pagina e ricomincia l’operazione, le proposte si perfezioneranno progressivamente a seconda della tua scelta di autori.</p>
 				<p>Questo messaggio sparirà quando seguirai 5 autori. 
-				Ti diamo appuntamento su "@auteurs" per ottenere dei suggerimenti di autori.</p>', # MODIF
+				Ti diamo appuntamento su "@auteurs" per ottenere dei suggerimenti di autori.</p>',
 	'modifier' => 'modifica',
 	'montrer_messages' => 'mostra i @total@ messaggi',
 
+	// N
+	'notif_me_nouveau' => '@nom_auteur@ ha pubblicato un nuovo post',
+	'notif_me_reponse' => '@nom_auteur@ ha risposto ad uno dei suoi post',
+	'notif_me_reponse_a' => '@nom_auteur@ ha risposto ad un post di @nom_auteur_init@',
+	'notif_message_footer' => '
+---------
+Per non ricevere più avvisi da @seenthis@,
+puoi impostare le tue preferenze nel tuo profilo
+@url@
+
+',
+	'notif_partage_texte' => 'Buongiorno @nom_dest@,
+
+@nom_aut_partage@ (@@login_aut_partage@) ha condiviso uno dei tuoi post su @seenthis@.',
+	'notif_partage_titre' => '@nom_aut_partage@ (@@login_aut_partage@) ha condiviso uno dei tuoi post su @seenthis@.',
+	'notif_suivremoi_texte' => 'Buongiorno @nom_dest@,
+
+@nom_aut@ (@@login_aut@) ti seguo su @seenthis@.',
+	'notif_suivremoi_titre' => '@nom_aut@ (@@login_aut@) ti seguo su @seenthis@.',
+
 	// P
 	'pave_accueil' => 'Dello <strong>short-blogging</strong> senza limite di caratteri. Delle <strong>raccomandazioni di link</strong>. Degli <strong>automatismi</strong> per redigere facilmente i tuoi messaggi. Dei <strong>forums</strong> sotto ogni post. Delle <strong>rassegne stampa d’attualità</strong>. Una <strong>tematizzazione</strong> avanzata.',
-	'people' => 'autori', # MODIF
+	'people' => 'autori',
 	'profil' => 'preferenze',
 	'profil_alerte_conversations' => '<b>conversazioni</b> / qualcuno risponde a un post a cui io stesso ho risposto',
-	'profil_alerte_dubien' => '<b>un amico che ti vuole bene</b> / qualcuno che mi segue', # MODIF
+	'profil_alerte_dubien' => '<b>un amico che ti vuole bene</b> / qualcuno che mi segue',
 	'profil_alerte_mes_billets' => '<b>mie pubblicazioni</b> / ricevere una copia dei miei post ',
-	'profil_alerte_nolife' => '<b>senza vita</b> / un autore che seguo risponde a tutte le pubblicazioni', # MODIF
-	'profil_alerte_nouveaux_billets' => '<b>nuovi post</b> / un nuovo post è stato pubblicato da un autore che seguo', # MODIF
+	'profil_alerte_nolife' => '<b>senza una vita</b> / un autore che seguo risponde a tutte le pubblicazioni',
+	'profil_alerte_nouveaux_billets' => '<b>nuovi post</b> / un nuovo post è stato pubblicato da un autore che seguo',
 	'profil_alerte_partage' => '<b>condivisione</b> / qualcuno ha condiviso il mio post',
 	'profil_alerte_reponse_partage' => '<b>risposta a una condivisione</b> / qualcuno ha rispoto a un post che ho condiviso',
 	'profil_alerte_reponses' => '<b>risposta ai miei post</b> / qualcuno ha risposto al mio post',
+	'profil_alerte_tag_suivi' => '<b>tag monitorato</b> / un nuovo post contiene un tag che seguo',
 	'profil_alertes' => 'Avvisi',
 	'profil_copyleft' => 'Copyright, Copyleft',
 	'profil_copyright_classique' => 'Nessuna licenza specifica (diritti d’autore predefiniti)',
@@ -84,7 +111,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'profil_licence' => 'Licenza dei miei messaggi',
 	'profil_liens_partage_fb' => 'Mostra i bottoni di condivisione verso Facebook',
 	'profil_liens_partage_tw' => 'Mostra i bottoni di condivisione verso Twitter',
-	'profil_logo' => 'Logo dell’autore (quadrato)', # MODIF
+	'profil_logo' => 'Logo dell’autore (quadrato)',
 	'profil_mexpedier' => 'Speditemi un email<br /> quando...',
 	'profil_partage' => 'Condividi',
 	'profil_rss' => 'Importare automaticamente un flusso d’articoli in formato ATOM o RSS',
@@ -97,17 +124,22 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'slogan_lien' => 'Iscriviti, è gratuito e rapido!',
-	'slogan_texte' => '<strong>Partecipa alla discussione!</strong> Su Seenthis, puoi citare degli articoli, commentarli, scrivere dei post, discutere con i tuoi amici, seguire degli autori che ti interessano...', # MODIF
+	'slogan_texte' => '<strong>Partecipa alla discussione!</strong> Su Seenthis, puoi citare degli articoli, commentarli, scrivere dei post, discutere con i tuoi amici, seguire degli autori che ti interessano...',
 	'suggestions' => 'Suggerimenti:',
 	'suivre_people' => 'Seguire @people@',
 	'suivre_url' => 'Seguire questo sito',
 	'suivre_url_stop' => 'Smettere di seguire questo sito',
 	'suivre_url_you' => 'Stai seguendo questo sito',
+	'supprimer_mon_profil' => 'Elimina il mio profilo',
+	'supprimer_mon_profil_au_revoir' => 'Eliminazione in corso... arrivederci!!!',
+	'supprimer_mon_profil_irreversible' => 'Elimina il mio profilo (irreversibile)',
+	'supprimer_mon_profil_login' => 'Inserisci il tuo login per confermare la richiesta',
+	'supprimer_veuillez_saisir' => 'Controlla il tuo login',
 
 	// T
 	'tags' => 'temi',
 	'theme_automatiquement' => 'Questo <b>tema</b> è stato generato <b>automaticamente</b>.',
-	'theme_manuellement' => 'Questo <b>tema</b> è attribuito <b> manualmente</b> dagli autori dei messaggi.', # MODIF
+	'theme_manuellement' => 'Questo <b>tema</b> è attribuito <b> manualmente</b> dagli autori dei messaggi.',
 	'themes_automatiques' => 'temi automatici',
 	'themes_vous_suivez' => 'I temi che segui',
 	'tous_les_messages_de' => 'Tutti i messaggi di @people@',
@@ -121,6 +153,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'votre_message' => 'Il tuo post',
 	'votre_message_public' => 'Il tuo messaggio pubblico a',
 	'vous_connaissez' => 'Potresti conoscere:',
-	'vous_suivent' => 'abbonati', # MODIF
+	'vous_suivent' => 'abbonati',
 	'vous_suivez' => 'abbonamenti'
 );
